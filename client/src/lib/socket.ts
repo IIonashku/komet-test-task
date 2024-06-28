db794b82dd1e7ca7bf1ddd56b613515f70d8896b
Ihor <ihor.ionashku@gmail.com>
2024-06-28 16:10:31 +0300

    update node env

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,5 +1,6 @@
 import { Socket, io } from "socket.io-client";
 
 const WSUrl = import.meta.env.VITE_WS_URL;
+const nodeEnvDev = import.meta.env.DEV;
 
-export const socket: Socket = io(WSUrl);
+export const socket: Socket = io(nodeEnvDev ? WSUrl : WSUrl);
