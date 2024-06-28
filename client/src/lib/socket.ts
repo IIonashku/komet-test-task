8b133da0cd1b9b3b41f89f4af24b6758cbf56a70
Ihor <ihor.ionashku@gmail.com>
2024-06-28 17:30:09 +0300

    test deployed BE

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { Socket, io } from "socket.io-client";
 
-const WSUrl = import.meta.env.VITE_WS_URL;
-const nodeEnvDev = import.meta.env.DEV;
+// const WSUrl = import.meta.env.VITE_WS_URL;
+// const nodeEnvDev = import.meta.env.DEV;
 
-export const socket: Socket = io(nodeEnvDev ? WSUrl : WSUrl);
+export const socket: Socket = io("https://websockets-task.onrender.com");
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   await app.listen(PORT);
 }
 bootstrap();
